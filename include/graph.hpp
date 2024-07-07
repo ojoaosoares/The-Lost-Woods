006442c4c7a914afa1a9352509e7063f36cc0de5
Leviathan <soaresjsantos93@gmail.com>
2024-07-07 17:46:40 -0300

    Refactor: Graph insertEdge function now has edges with weight.

--- include/graph.hpp
+++ include/graph.hpp
@@ -4,19 +4,19 @@
 #include "singlylinkedlistunordered.hpp"
 
 
-template <typename T>
+template <typename T, typename Weight>
 class Graph
 {
     private:
         T *vertices;
-        SinglyLinkedListUnordered<long long> *adjacencyLists;
+        SinglyLinkedListUnordered<std::pair<long long, Weight> *adjacencyLists;
         
     public:
         Graph(long long v);
         ~Graph();
 
         void insertVertice(long long index, T item);
-        void insertEdge(long long v1, long long v2);
+        void insertEdge(long long v1, long long v2, Weight W);
 
 };
 
--- src/graph.cpp
+++ src/graph.cpp
@@ -1,25 +1,26 @@
 #include "graph.hpp"
 
-template <typename T>
-Graph<T>::Graph(long long v) {
+template <typename T, typename Weight>
+Graph<T, Weight>::Graph(long long v) {
     vertices = new T[v];
     adjacencyLists = new SinglyLinkedListUnordered[v];
 }
 
-template <typename T>
-Graph<T>::~Graph() {
+template <typename T, typename Weight>
+Graph<T, Weight>::~Graph() {
     delete[] vertices;
     delete[] adjacencyLists;
 }
 
-template <typename T>
-void Graph<T>::insertVertice(long long index, T item) {
+template <typename T, typename Weight>
+void Graph<T, Weight>::insertVertice(long long index, T item) {
     vertices[index] = item;
 }
 
 
-template <typename T>
-void Graph<T>::insertEdge(long long v1, long long v2) {
-    adjacencyLists[v1].insertEnd(v2);
+template <typename T, typename Weight>
+void Graph<T, Weight>::insertEdge(long long v1, long long v2, Weight w) {
+    std::pair<long long, Weight> newEdge(v1, w);
+    adjacencyLists[v1].insertEnd(newEdge);
 }
 
