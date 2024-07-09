e72dc09c528d261b6d16a39e1c1cd3730754de90
Leviathan <soaresjsantos93@gmail.com>
2024-07-09 00:45:39 -0300

    Fix: Fixing hash

--- src/hash.cpp
+++ src/hash.cpp
@@ -2,5 +2,5 @@
 
 template<typename T>
 long long Hash_Lost_woods<T>::operator() (const Tuple<Tuple<long long, long long>, T>& key, const long long &max) const {
-    return static_cast<std::size_t> (max / key.first.first + key.first.second);
+    return (max / key.first.first + key.first.second);
 }
