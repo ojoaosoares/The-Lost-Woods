e4dfc776c0f6516dd2ef095b8df84d2de3bb6302
Leviathan <soaresjsantos93@gmail.com>
2024-07-09 22:00:07 -0300

    Refactor: Refactoring comp class.

--- include/comparator.hpp
+++ include/comparator.hpp
@@ -7,7 +7,7 @@ template<typename T>
 class Comp_Lost_woods {
 
     public:
-        bool operator() (const Tuple<Tuple<long long, long long>, T>& v1, const Tuple<Tuple<long long, long long>, T>& v2) const;
+        bool operator() (const T& v1, const T& v2) const;
 
 };
 
